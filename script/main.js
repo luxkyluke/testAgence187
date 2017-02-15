006f17f3b389e59a9a1c2c7715bbf110cf768962
luxkyluke <rock_on77500@hotmail.fr>
2017-02-15 15:11:52 +0100

    main

---
+++ script/main.js
@@ -0,0 +1,115 @@
+const query = "http://maps.googleapis.com/maps/api/geocode/json?address="
+const regexZipcode = new RegExp("[0-9]");
+
+
+function checkZipcode(element, e){
+	e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+	const zipcode =  element["zipcode"].value;
+
+	//check lenght of the zipcode
+	if((zipcode.length>0 && zipcode.length<5) || !regexZipcode.test(zipcode) ){
+		addClass(document.querySelector("#zipcode_txt"), "error");
+		return false;
+	}
+
+	//cleaning older results
+	document.getElementById("results").innerHTML = "";
+	
+	//getting the resulting city and loading it
+	getCity(zipcode, function(city){
+		loadCity(city, function(){
+			animMarker();
+		});
+	});
+}
+
+function animMarker(){
+	setTimeout(function(){
+		document.getElementById("location").className += "visible";
+	}, 0);
+
+	var results = document.getElementById("results");
+	var location = document.getElementById("location");
+
+	var resultsWidth 	= results.offsetWidth;	
+	var resultsHeight	= results.offsetHeight;
+	var locationWidth 	= location.offsetWidth;
+	var locationHeight 	= location.offsetHeight;
+
+	const padding = 10;
+
+	var maxX = resultsWidth - locationWidth - padding;
+	var minX = locationWidth + padding;
+
+	var maxY = resultsHeight - (locationHeight*2) - padding;
+	var minY = locationHeight + padding;
+
+	var x = Math.floor((Math.random() * maxX) + minX);
+	var y = Math.floor((Math.random() * maxY) + minY);
+
+
+	location.setAttribute("style", "transform: translate("+-x+"px, "+y+"px);");
+	console.log("translate("+-x+"px, "+y+"px)");
+	console.log("results : "+resultsWidth +" "+resultsHeight);
+	console.log("location : "+locationWidth +" "+locationHeight);
+
+}
+
+function getCity(zipcode, callback){
+	var url = query+zipcode;
+	getAjax(url, function(res){
+		var result = JSON.parse(res);
+		var city = result.results[0].address_components[1].long_name;
+		callback(city);
+	});		
+}
+
+function load(html, cities, _callback){
+	var str = new String(html);
+	var tmp = str.split("ResultCity");
+	str = tmp[0]+cities[0]+tmp[1];
+	var results = document.getElementById("results");
+	results.innerHTML += str;
+	_callback();
+}
+
+function loadCity(city, _callback){
+	//loading city markers
+	getAjax("marker.html", function(html){
+		var str = new String(html);
+		var tmp = str.split("ResultCity");
+		str = tmp[0]+city+tmp[1];
+		var results = document.getElementById("results");
+		results.innerHTML += str;
+		_callback();
+	});
+
+}
+
+//Key event
+function handleKeyPressed(e){
+	const key = (e.which) ? e.which : e.keyCode;
+	if (key != 46 && key > 31 
+            && (key < 48 || key > 57)){
+		e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+		return false;
+	}
+
+	//reset error status 
+	if(hasClass(document.querySelector("#zipcode_txt"), "error"))
+		removeClass(document.querySelector("#zipcode_txt"), "error");
+}
+
+
+/**********************************AJAX**********************************/
+
+var getAjax = function(url, callback){
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function () {
+		if (xmlHttp.readyState === 4 && xmlHttp.status === 200){
+			callback(xmlHttp.responseText);
+		}		
+	}
+	xmlHttp.open('GET', url, true);
+	xmlHttp.send(null);
+}
---
+++ script/utility.js
@@ -0,0 +1,15 @@
+function hasClass(el, name) {
+   return new RegExp('(\\s|^)'+name+'(\\s|$)').test(el.className);
+}
+
+function addClass(el, name)
+{
+   if (!hasClass(el, name)) { el.className += (el.className ? ' ' : '') +name; }
+}
+
+function removeClass(el, name)
+{
+   if (hasClass(el, name)) {
+      el.className=el.className.replace(new RegExp('(\\s|^)'+name+'(\\s|$)'),' ').replace(/^\s+|\s+$/g, '');
+   }
+}
